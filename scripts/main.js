af1a78e7042fcc064b4a6b85c61df1e179a34ee0
jremya <remya.jacobn@gmail.com>
2016-02-11 15:59:58 +1100

    New foder for scripts

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+var progressbars = [
+            { name: 'progress1', amount: 0 },
+            { name: 'progress2', amount: 0 },
+			{ name: 'progress3', amount: 0 }
+        ];
+		
+var ractive = new Ractive({
+    el: '#container',
+    template: '#template',
+    data: {
+        progressbars: progressbars,
+		selectedProgress: 0,
+        amounts: [ +25, +10, -10, -25 ]
+    },
+    adjust: function ( d ) {
+        var selected = this.get( 'selectedProgress' );
+		
+        var keypath = 'progressbars[' + selected + '].amount';
+		
+        this.add( keypath, d );	
+
+		if(ractive.get('progressbars[' + selected + '].amount') < 0 ){
+			ractive.set('progressbars[' + selected + '].amount', 0);	
+		}				
+    }
+});
